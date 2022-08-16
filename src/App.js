8a98cf39bd8a41174bf5615d302aaec7bf343136
TataKor <107408600+TataKor@users.noreply.github.com>
2022-08-16 21:38:30 +0300

    Include bootstrap

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <button className="btn btn-primary">Hello</button>
       <footer>
         This project was coded by <a href="https://fabulous-sherbet-795727.netlify.app" target="_blank"> TataKor</a> and is{" "}
       <a href="https://github.com/TataKor/react-weather-app"
