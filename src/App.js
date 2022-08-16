6fb1c4124a1a9401f915eb1a0c8626116b605355
TataKor <107408600+TataKor@users.noreply.github.com>
2022-08-16 16:37:18 +0300

    url remove

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by <a href="https://fabulous-sherbet-795727.netlify.app" target="_blank"> TataKor</a> and is{" "}
+      <a href="https://github.com/TataKor/react-weather-app"
+      target="_blank">
+        open-sourced on GitHub.
+      </a>
+      </footer>
     </div>
   );
 }
