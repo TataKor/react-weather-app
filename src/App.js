2edddcd3fbf91bdf420702b634e06b4d48d964af
TataKor <107408600+TataKor@users.noreply.github.com>
2022-08-17 12:04:17 +0300

    Added weather form

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import Weather from './Weather';
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-      <h1>Weather App</h1>
+      <Weather />
       <footer>
         This project was coded by <a href="https://fabulous-sherbet-795727.netlify.app" target="blank"> TataKor</a> and is{" "}
       <a href="https://github.com/TataKor/react-weather-app"
